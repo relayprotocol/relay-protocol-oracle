1127b2f58618a458dfb90aec489c6cc881a4e2a8
lljxx1 <lljxx1@gmail.com>
2025-07-15 09:21:17 +0800

    feat: move bitcoin rpc to sdk

--- src/common/vm/bitcoin-vm/rpc.ts
+++ src/common/vm/bitcoin-vm/rpc.ts
@@ -1,16 +1,7 @@
-import { MempoolSpaceConnection } from "./utils/mempool-space-connection";
-import { RpcConnection } from "./utils/rpc-connection";
+import { bitcoin } from "@reservoir0x/relay-protocol-sdk";
 import { getChain } from "../../chains";
-
-const createProvider = (rpcUrl: string) => {
-  if (rpcUrl.includes("mempool.space")) {
-    return new MempoolSpaceConnection(rpcUrl);
-  }
-  return new RpcConnection(rpcUrl);
-};
 
 export const httpRpc = async (chainId: string) => {
   const chain = await getChain(chainId);
-  return createProvider(chain.httpRpcUrl);
+  return bitcoin.createProvider(chain.httpRpcUrl);
 };
-  
