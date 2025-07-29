c6cb734c42471cee6e24bd78e49f91daba3b95f4
George Roman <30772943+georgeroman@users.noreply.github.com>
2025-07-29 13:53:09 +0200

    feat: setup swagger

--- src/http-server.ts
+++ src/http-server.ts
@@ -1,3 +1,5 @@
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
 import Fastify from "fastify";
 
@@ -9,37 +11,75 @@ const COMPONENT = "http-server";
 
 const httpServer = Fastify().withTypeProvider<TypeBoxTypeProvider>();
 
-// Setup authentication
-httpServer.addHook("preHandler", (req, reply, done) => {
-  if (config.apiKeys) {
-    const apiKey = req.headers["x-api-key"] as string | undefined;
-    if (!apiKey || !config.apiKeys[apiKey]) {
-      return reply.code(401).send({ error: "Unauthorized" });
-    }
-  }
+// Setup swagger
+const setupSwagger = async () => {
+  await httpServer.register(fastifySwagger, {
+    mode: "dynamic",
+    openapi: {
+      info: {
+        title: "Relay Protocol Oracle API",
+        version: "v1",
+      },
+    },
+  });
+  await httpServer.register(fastifySwaggerUi, {
+    routePrefix: "/documentation",
+    uiConfig: {
+      docExpansion: "full",
+      deepLinking: false,
+    },
+    uiHooks: {
+      onRequest: function (_request, _reply, next) {
+        next();
+      },
+      preHandler: function (_request, _reply, next) {
+        next();
+      },
+    },
+    staticCSP: true,
+    transformStaticCSP: (header) => header,
+    transformSpecification: (swaggerObject, _request, _reply) => {
+      return swaggerObject;
+    },
+    transformSpecificationClone: true,
+  });
+};
 
-  return done();
+setupSwagger().then(() => {
+  // Setup authentication
+  httpServer.addHook("preHandler", (req, reply, done) => {
+    if (config.apiKeys) {
+      const apiKey = req.headers["x-api-key"] as string | undefined;
+      if (!apiKey || !config.apiKeys[apiKey]) {
+        return reply.code(401).send({ error: "Unauthorized" });
+      }
+    }
+
+    return done();
+  });
+
+  // Setup endpoints
+  setupEndpoints(httpServer);
+
+  // Start listening
+  httpServer.listen(
+    {
+      host: "0.0.0.0",
+      port: config.httpPort,
+    },
+    (error) => {
+      if (error) {
+        logger.error(
+          COMPONENT,
+          JSON.stringify({
+            msg: `Failed to start http server: ${error}`,
+            stack: error?.stack,
+          })
+        );
+        process.exit(1);
+      }
+
+      logger.info(COMPONENT, JSON.stringify({ msg: "Http server started" }));
+    }
+  );
 });
-
-setupEndpoints(httpServer);
-
-httpServer.listen(
-  {
-    host: "0.0.0.0",
-    port: config.httpPort,
-  },
-  (error) => {
-    if (error) {
-      logger.error(
-        COMPONENT,
-        JSON.stringify({
-          msg: `Failed to start http server: ${error}`,
-          stack: error?.stack,
-        })
-      );
-      process.exit(1);
-    }
-
-    logger.info(COMPONENT, JSON.stringify({ msg: "Http server started" }));
-  }
-);
