a54097d90fd927b9d00e2ef9b402ae714002aa44
George Roman <30772943+georgeroman@users.noreply.github.com>
2025-10-14 14:13:05 +0200

    tweak: fix transform pattern

--- jest.config.ts
+++ jest.config.ts
@@ -8,6 +8,10 @@ import type { Config } from "jest";
 const config: Config = {
   verbose: true,
 
+  transform: {
+    "^.+\\.(t|j)s?$": "ts-jest",
+  },
+
   transformIgnorePatterns: ["node_modules/(?!(@nktkas/hyperliquid|@noble)/)"],
 
   // globalSetup: "./test/setup.ts",
