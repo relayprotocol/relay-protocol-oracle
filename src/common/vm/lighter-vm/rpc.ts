0f1a3d13198d64c747b4b7562fef4c5815c0e335
George Roman <30772943+georgeroman@users.noreply.github.com>
2025-12-16 15:31:06 +0100

    chore: configure lighter (#97)

--- src/common/vm/lighter-vm/rpc.ts
+++ src/common/vm/lighter-vm/rpc.ts
@@ -5,7 +5,14 @@ import { getChain } from "../../chains";
 export const httpRpc = async (chainId: string) => {
   const chain = await getChain(chainId);
 
-  const apiClient = new ApiClient({ host: chain.httpRpcUrl });
+  const additionalHeaders = process.env.LIGHTER_PROXY_API_KEY
+    ? { "x-api-key": process.env.LIGHTER_PROXY_API_KEY }
+    : undefined;
+
+  const apiClient = new ApiClient({
+    host: chain.httpRpcUrl,
+    additionalHeaders,
+  });
   const transactionApi = new TransactionApi(apiClient);
 
   return {
