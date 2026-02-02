1af975f677745bf1e528cfd88872353afbc2553c
George Roman <30772943+georgeroman@users.noreply.github.com>
2026-02-02 14:14:36 +0100

    feat: add support for rpcs with basic authorization (#147)

--- src/common/vm/ethereum-vm/rpc.ts
+++ src/common/vm/ethereum-vm/rpc.ts
@@ -4,6 +4,22 @@ import { getChain } from "../../chains";
 
 export const httpRpc = async (chainId: string) => {
   const chain = await getChain(chainId);
+
+  // Handle RPCs which require basic authorization
+  let basicAuthCredentials: { user: string; password: string } | undefined;
+  const needsBasicAuth = chain.httpRpcUrl.includes("@");
+  if (needsBasicAuth) {
+    try {
+      const parsedUrl = new URL(chain.httpRpcUrl);
+      basicAuthCredentials = {
+        user: parsedUrl.username,
+        password: parsedUrl.password,
+      };
+    } catch {
+      // Skip errors
+    }
+  }
+
   return createPublicClient({
     chain: {
       // We only need to `rpcUrls`, but viem makes all the other ones mandatory
@@ -20,6 +36,23 @@ export const httpRpc = async (chainId: string) => {
         },
       },
     },
-    transport: http(),
+    transport: http(
+      chain.httpRpcUrl,
+      basicAuthCredentials
+        ? {
+            fetchOptions: {
+              headers: {
+                Authorization:
+                  "Basic " +
+                  Buffer.from(
+                    basicAuthCredentials.user +
+                      ":" +
+                      basicAuthCredentials.password,
+                  ).toString("base64"),
+              },
+            },
+          }
+        : undefined,
+    ),
   });
 };
